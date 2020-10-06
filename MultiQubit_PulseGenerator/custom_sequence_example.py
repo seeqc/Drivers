96edbd31f768f101ab888bab84a16d735cef386d
Chris Checkley <ccheckley@seeqc.com>
2020-10-06 11:55:35 +0100

    Update custom_sequence_example.py
    Improved formatting

--- MultiQubit_PulseGenerator/custom_sequence_example.py
+++ MultiQubit_PulseGenerator/custom_sequence_example.py
@@ -59,34 +59,37 @@ pulse.amplitude = 1
 pulse.frequency = 100e6
 test_waveform.sample_rate = 1200e6
 
-test_waveform.pulses_1qb_xy[0]=pulse
-test_waveform.pulses_1qb_z[0]=pulse
+test_waveform.pulses_1qb_xy[0] = pulse
+test_waveform.pulses_1qb_z[0] = pulse
 
-test_waveform.perform_predistortion_z=False
+test_waveform.perform_predistortion_z = False
 
-test_waveform.number_readout_waveforms=1
-test_waveform.readout_match_main_size=False
-test_waveform.readout_i_offset=0
-test_waveform.readout_i_offset2=0
-test_waveform.readout_q_offset=0
-test_waveform.readout_q_offset2=0
-test_waveform.readout_predistort=False
+test_waveform.number_readout_waveforms = 1
+test_waveform.readout_match_main_size = False
+test_waveform.readout_i_offset = 0
+test_waveform.readout_i_offset2 = 0
+test_waveform.readout_q_offset = 0
+test_waveform.readout_q_offset2 = 0
+test_waveform.readout_predistort = False
 test_waveform.readout_trig_duration = 0.0
 
-test_waveform.readout.frequencies[0]=30e6
+test_waveform.readout.frequencies[0] = 30e6
 test_waveform.readout.iq_ratio = 1
 test_waveform.readout.iq_skew = 1 * np.pi / 180
 test_waveform.readout.n_records = 1
 
 test_waveform.readout_delay = 0
 
-phases = 2 * np.pi * np.array([0.8847060, 0.2043214, 0.9426104, 0.6947334, 0.8752361, 0.2246747,0.6503154, 0.7305004, 0.1309068])
+phases = 2 * np.pi * np.array([
+    0.8847060, 0.2043214, 0.9426104, 0.6947334, 0.8752361, 
+    0.2246747,0.6503154, 0.7305004, 0.1309068
+])
 
 
 pulse_readout = pulses.Square(complex=True)
 pulse_readout.plateau = 500e-9
-pulse_readout.frequency=30e6
-pulse_readout.amplitude=1
+pulse_readout.frequency = 30e6
+pulse_readout.amplitude = 1
 test_waveform.pulses_readout[0] = pulse_readout
 
 
@@ -94,5 +97,6 @@ test_waveform.pulses_readout[0] = pulse_readout
 
 waveform=test_waveform.get_waveforms(test_sequence)
 
+###Plot real part of XY waveform###
 
 plt.plot(np.real(waveform['xy']).T)
